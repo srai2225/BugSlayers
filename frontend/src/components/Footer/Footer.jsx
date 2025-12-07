84f4caf604ee376fc201d3a1d4db348a1dbdaeb9
Sumit Rai <raisumit2225.com>
2025-12-07 19:02:34 +0530

    first and last commit

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -39,7 +39,7 @@ const Footer = () => {
       </div>
       <hr />
       <p className="footer-copyright">
-        {/* Copyright 2024 @ Tomato.com - All Right Reserved. */}
+        {/* Copyright 2024*/}
       </p>
     </div>
   );
